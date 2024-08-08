d1e9aaed8a83b98f3b714b63359d85cf7faa9025
Tarun <92327755+TarunNehla@users.noreply.github.com>
2024-08-08 19:56:52 +0530

    Update Day 01.cpp

--- Day 01.cpp
+++ Day 01.cpp
@@ -82,3 +82,62 @@ vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int ne
 
         return ans;
     }
+
+
+
+
+
+
+class Solution {
+    
+    void solve(int x, int y,vector<vector<int>> &mat, set<string>&st, vector<vector<int>>&vis, string &path){
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        if(x==n-1 && y==n-1){
+            st.insert(path);
+            return;
+        }
+        vis[x][y] = 1;
+        vector<int>dx = {-1,1,0,0};
+        vector<int>dy = {0,0,-1,1};
+        vector<char>dp = {'U','D','L','R'};
+        for(int i = 0; i<4;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx<0 || nx>=n || ny<0 || ny>=m || !mat[nx][ny] || vis[nx][ny]) continue;
+            path += dp[i];
+            solve(nx,ny,mat,st,vis,path);
+            path.pop_back();
+        }
+        vis[x][y] = 0;
+    }
+    
+    
+    
+  public:
+    vector<string> findPath(vector<vector<int>> &mat) {
+        // Your code goes here
+        vector<string>ans;
+        if(!mat[0][0]){
+            ans.push_back("-1");
+            return ans;
+        } 
+        
+        set<string>st;
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        string path  = "";
+        solve(0,0,mat,st,vis,path);
+        
+        if(st.size()==0){
+            ans.push_back("-1");
+            return ans;
+        } 
+        for(auto it:st){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
